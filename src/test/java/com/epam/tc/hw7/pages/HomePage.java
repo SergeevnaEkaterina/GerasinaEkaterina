6243fb892b78be00579cf1f2f43517eae954e28f
Ekaterina Gerasina <gerasinaes@mail.ru>
2021-11-27 12:30:36 +0300

    fixes

--- src/test/java/com/epam/tc/hw7/pages/HomePage.java
+++ src/test/java/com/epam/tc/hw7/pages/HomePage.java
@@ -1,13 +1,17 @@
 package com.epam.tc.hw7.pages;
 
 import com.epam.jdi.light.elements.common.Label;
+import com.epam.jdi.light.elements.complex.Menu;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.Icon;
 import com.epam.tc.hw7.components.LoginForm;
 import com.epam.tc.hw7.model.User;
+import io.qameta.allure.Step;
 
 @Url("/index.html")
 @Title("Home Page")
@@ -18,8 +22,24 @@ public class HomePage extends WebPage {
     public static Icon userIcon;
     @Css("#user-name")
     public static Label userName;
+    @UI(".m-l8 li")
+    public static Menu headerMenu;
 
-    public static void loginAs(User user) {
+    @UI(".btn-login")
+    public static Button logout;
+
+    public void logout() {
+        userIcon.click();
+        logout.click();
+    }
+
+    @Step("Open Metals & Colors page by {string} menu")
+    public void selectMenu(String value) {
+        headerMenu.select(value);
+    }
+
+    public void loginAs(User user) {
+        open();
         userIcon.click();
         loginForm.loginAs(user);
     }
--- src/test/java/com/epam/tc/hw7/pages/MetalsAndColorsPage.java
+++ src/test/java/com/epam/tc/hw7/pages/MetalsAndColorsPage.java
@@ -1,6 +1,6 @@
 package com.epam.tc.hw7.pages;
 
-import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
@@ -9,7 +9,6 @@ import com.epam.tc.hw7.components.MetalsAndColorsForm;
 import io.qameta.allure.Step;
 import java.util.ArrayList;
 import java.util.List;
-import org.openqa.selenium.WebElement;
 
 @Url("/metals-colors.html")
 @Title("Metal and Colors")
@@ -19,12 +18,12 @@ public class MetalsAndColorsPage extends WebPage {
     public static MetalsAndColorsForm metalsAndColorsForm;
 
     @Css(".results li")
-    private static List<UIElement> logs;
+    private static List<Label> logs;
 
     @Step("Get result section logs")
     public static List<String> listLogs() {
         List<String> logsText = new ArrayList<>();
-        for (WebElement element : logs) {
+        for (Label element : logs) {
             System.out.println(element.getText());
             logsText.add(element.getText());
         }
